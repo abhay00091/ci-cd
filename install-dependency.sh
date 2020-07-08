eba1b028cce1663005970bddb81fa356f6af8efe
abhay00091 <abhay00091@gmail.com>
2020-07-08 02:44:27 -0400

    updated file

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing Dependencies...."
 sudo apt-get update -y
-sudo install -y default-jre docker.io docker-compose
+sudo apt-get install -y default-jre docker.io docker-compose
